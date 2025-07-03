35cc450327cbc631b672ac6694319ac80bd0347e
YukioTachibana <tachibanayukio6@gmail.com>
2025-07-03 22:12:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 import os
 from datetime import datetime
 from werkzeug.utils import secure_filename
@@ -11,21 +10,12 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///yukify.db'
 app.config['UPLOAD_FOLDER'] = 'uploads'
 
 db = SQLAlchemy(app)
-login_manager = LoginManager(app)
-
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
 
 class MediaFile(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     filename = db.Column(db.String(255), nullable=False)
     filetype = db.Column(db.String(20), nullable=False)
     upload_time = db.Column(db.DateTime, default=datetime.utcnow)
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -34,13 +24,13 @@ def index():
         if file:
             filename = secure_filename(file.filename)
             ext = filename.rsplit('.', 1)[1].lower()
-            path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-            file.save(path)
-            new_file = MediaFile(filename=filename, filetype=ext)
-            db.session.add(new_file)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            db.session.add(MediaFile(filename=filename, filetype=ext))
             db.session.commit()
             return redirect(url_for('index'))
+
     files = MediaFile.query.order_by(MediaFile.upload_time.desc()).all()
     return render_template('index.html', files=files)
 
@@ -52,5 +42,3 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(host='0.0.0.0', port=10000)
-
-
